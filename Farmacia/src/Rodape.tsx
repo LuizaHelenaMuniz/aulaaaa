80424386740124d846c2cf2c613fefae9446d3c4
helenamuniz2008-a11y <helenamuniz2008@gmsil.com>
2026-02-02 16:22:11 -0300

    leroelrolero

--- Farmacia/src/Rodape.tsx
+++ Farmacia/src/Rodape.tsx
@@ -1,9 +1,10 @@
 function Rodape() {
-    return
+    return(
     <>
     <footer>
         <img src="https://wwfafrica.awsassets.panda.org/img/emperor-penguin_original_ww1446916_169800.webp"></img>
     </footer>
     </>
+    )
 }
 export default Rodape
