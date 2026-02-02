35f1b85c47474ecbdf0b43f78275e16e05135a87
helenamuniz2008-a11y <helenamuniz2008@gmsil.com>
2026-02-02 15:50:30 -0300

    adicionando imagem

---
+++ Farmacia/src/Topo.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return
+    <>
+    <header>
+        <img src="https://wwfafrica.awsassets.panda.org/img/emperor-penguin_original_ww1446916_169800.webp"></img>
+    </header>
+    </>
+}
+export default Topo
